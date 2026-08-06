37c9753c6fcd33858dc6daba25e1d19566461144
Emmanuel Oluyemi <e8060097@gmail.com>
2026-08-06 19:53:45 +0100

    integrating seo

--- AttendEase/settings.py
+++ AttendEase/settings.py
@@ -231,7 +231,7 @@ LOGGING = {
 }
 
 # Email Configuration for Contact Form
-EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.console.EmailBackend')
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', '587'))
 EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
